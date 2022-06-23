b44fa0f48433022846dc2dbf52ea5c5f7d0be375
TJ <thaddeusjiang@gmail.com>
2022-06-23 14:20:17 +0900

    Use react-app-rewired to modify the webpack config

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,22 @@
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  return config;
+/* config-overrides.js */
+const {
+  rewiredSingleSpa,
+  rewiredSingleSpaDevServer,
+} = require("react-app-rewired-single-spa");
+
+// use `customize-cra`
+const { override, overrideDevServer } = require("customize-cra");
+
+module.exports = {
+  webpack: override(
+    rewiredSingleSpa({
+      orgName: "thaddeusjiang",
+      projectName: "legacy",
+      entry: "./src/thaddeus-legacy.js",
+      reactPackagesAsExternal: true,
+      peerDepsAsExternal: true,
+      orgPackagesAsExternal: true,
+    })
+  ),
+  devServer: overrideDevServer(rewiredSingleSpaDevServer()),
 };
